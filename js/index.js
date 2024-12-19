c19a2aa653fa6b21b9be441a97792e1a9e25d64a
JenyaCherny <jenya.cherny@gmail.com>
2024-12-18 21:13:27 -0500

    experimented with data types in index.js and commented out my code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*const two = 2;
+console.log(two);
+console.log(typeof two);
 
+const num = 3.5;
+console.log(num);
+console.log(typeof num);
+
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+const myName = "Anna";
+console.log(myName);
+console.log(typeof myName);
+
+const schoolName = "Manual High";
+console.log(schoolName);
+console.log(typeof schoolName);*/
